5fdcdcd68b911ef1b6962c0453d806d7b2c24c58
Quentin Machard <quentin.machard@idix.fr>
2017-05-15 11:42:31 +0200

    Create ApiProblem object to Manage Problems

---
+++ Api/ApiProblemException.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quentinmachard
+ * Date: 21/07/2016
+ * Time: 16:22
+ */
+
+namespace Paddix\CoreBundle\Api;
+
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ApiProblemException extends HttpException {
+	private $apiProblem;
+
+	/**
+	 * ApiProblemException constructor.
+	 * @param ApiProblem $apiProblem
+	 * @param \Exception|NULL $previous
+	 * @param array $headers
+	 * @param int $code
+	 */
+	public function __construct(ApiProblem $apiProblem, \Exception $previous = null, array $headers = array(), $code = 0) {
+		$this->apiProblem = $apiProblem;
+		$statusCode = $apiProblem->getStatusCode();
+		$message = $apiProblem->getTitle();
+		parent::__construct($statusCode, $message, $previous, $headers, $code);
+	}
+
+	/**
+	 * @return ApiProblem
+	 */
+	public function getApiProblem() {
+		return $this->apiProblem;
+	}
+}
